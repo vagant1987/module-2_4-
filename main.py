452622eca9257f3782b6a4206939745ed1f2ca57
vagant1987 <vogant87@mail.ru>
2024-10-27 16:36:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 n = 0
 primes = []  # список простых чисел
 not_primes = []  # список составных чисел
-
-for num in numbers:
-    if num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
-                not_primes.append(num)
-                break
-
-        else:
-            primes.append(num)
+for i in range(len(numbers)):
+    is_prime = True  # признак простого числа
+    #    k = 0
+    n = numbers[i]  # Проверямое число
+# используем алгоритм "Перебор делителей" для определения простого  числа
+    if n < 2:   # не 0 и не 1
+        print(n, '- не простое и не сложное число')
+        continue
     else:
-        not_primes.append(num)
-print('Список простых чисел:', primes)
-print('Список не простых чисел:', not_primes)
-
+        f = n ** (1 / 2)  # Корень квадратный из n
+    for a in range(2, int(f + 1)):
+        if n % a == 0:
+            is_prime = False
+            break
+    if not (is_prime):
+        not_primes.append(n)
+    else:
+        primes.append(n)
+is_prime = True
+print('Простые числа ', primes)
+print('Составные числа', not_primes)
 
 
 
